da978bd3f6e2e6953bb094e4571a4fb0e21deffb
Kostikans <48389219+Kostikans@users.noreply.github.com>
2020-12-02 07:54:05 +0300

    Update loggermiddleware.go

--- package/middlewareApi/loggermiddleware.go
+++ package/middlewareApi/loggermiddleware.go
@@ -40,7 +40,7 @@ func LoggerMiddleware(log *logger.CustomLogger, metrics *metrics.PromMetrics) mu
 
 			respTime := time.Since(start)
 			log.EndReq(respTime.Microseconds(), req.Context())
-
+			fmt.Println(srw.statusCode)
 			if req.RequestURI != "/api/v1/metrics" {
 				metrics.Hits.WithLabelValues(strconv.Itoa(srw.statusCode), req.URL.String(), req.Method).Inc()
 				metrics.Total.Add(1)
